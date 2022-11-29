1007379ef97321420f2be8ef5e2cf84b5941ec8f
Emmanuel Hateka <emmanuelhateka.com>
2022-11-29 04:37:47 +0000

    completed topbar component for now

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,16 +1,41 @@
 import React from "react";
-import { CiSearch } from "react-icons/ci";
+import "../styles/topBar.css";
+import { CiSearch, CiDark } from "react-icons/ci";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { AiFillCaretDown } from "react-icons/ai";
+
+import { Badge } from "@mui/material";
 
 const TopBar = () => {
   return (
     <div className="topBar">
       <div className="left">
         <div className="searchCont">
-          <input type="text" />
-          <CiSearch />
+          <input type="text" placeholder="Search" />
+          <div>
+            <CiSearch />
+          </div>
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="icon">
+          <CiDark />
+        </div>
+        <div className="icon">
+          <Badge color="secondary" variant="dot">
+            <IoIosNotificationsOutline />
+          </Badge>
+        </div>
+        <div className="account">
+          <div className="profileImgCont">
+            <img src="https://i.ibb.co/dGcxdHw/intern-img-jg.png" alt="" />
+          </div>
+          <span>Cyklone Hateka</span>
+          <div className="icon">
+            <AiFillCaretDown />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import TopBar from "../components/TopBar";
 import "../styles/dashboard.css";
+import StatsWidget from "../components/StatsWidget";
 const Dashboard = () => {
   return (
     <div className="dashboard">
       <div className="dashboardCont">
         <TopBar />
+        <div className="statsWidgetCont">
+          <StatsWidget />
+          <StatsWidget />
+          <StatsWidget />
+          <StatsWidget />
+        </div>
       </div>
     </div>
   );
