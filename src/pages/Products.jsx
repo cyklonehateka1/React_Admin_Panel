399bb4fb9a07d5dd5b7515c44f218cea0da51456
Emmanuel Hateka <emmanuelhateka.com>
2022-12-04 03:53:04 +0000

    started products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Table from "../components/Table";
+import "../styles/products.css";
+import { ordersPageColumns, ordersPageRows } from "../utils/data";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div className="products">
+      <div className="productCont">
+        <Table
+          rows={ordersPageRows}
+          columns={ordersPageColumns}
+          pagination={10}
+          pageSize={10}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Products;
