bace64f2016a327c27d3d46ecb39669f901dbee5
Emmanuel Hateka <emmanuelhateka.com>
2022-12-06 05:12:30 +0000

    styled order products table

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,67 +1,55 @@
 import React from "react";
 import "../styles/order.css";
+import Table from "./Table";
+import { orderProductsCol, orderProductRow } from "../utils/data";
 
 const Order = () => {
   return (
     <div className="order">
       <div className="orderCont">
         <h5>Order from Cyklone Hateka</h5>
-        <div>
-          <div>
+        <div className="orderTopBar">
+          <div className="first">
             <span>Order Id</span>
             <h5>424867236643</h5>
           </div>
-          <div>
+          <div className="second">
             <span>Custmer ID</span>
             <h5>7232445354657</h5>
           </div>
         </div>
-        <h4 className="orderInfo">Order Info</h4>
+        <h4 className="orderInfoH">Order Info</h4>
         <div className="orderInfoCont">
           <div>
-            <div>
+            <div className="first">
               <span>Estimated Shipping Cost</span>
               <h5>$3984</h5>
             </div>
-            <div>
+            <div className="second">
               <span>Address</span>
               <h5>Accra, Airport City</h5>
             </div>
           </div>
           <div>
-            <div>
-              <span>Estimated Shipping Cost</span>
-              <h5>$3984</h5>
-            </div>
-            <div>
-              <span>Address</span>
-              <h5>Accra, Airport City</h5>
-            </div>
-          </div>
-          <div>
-            <div>
+            <div className="first">
               <span>Payment Method</span>
               <h5>Debit Card (MaterCard)</h5>
             </div>
-            <div>
+            <div className="second">
               <span>Account Number</span>
               <h5>1400005266928</h5>
             </div>
           </div>
-          <div className="productsInfoCont">
-            <h4>Products</h4>
-            <div className="products">
-              <table>
-                <thead>
-                  <th>Name</th>
-                  <th>Qty</th>
-                  <th>Size</th>
-                  <th>Image</th>
-                  <th>Price</th>
-                  <th>Color</th>
-                </thead>
-              </table>
-            </div>
+        </div>
+        <div className="productsInfoCont">
+          <h4>Products</h4>
+          <div className="products">
+            <Table
+              rows={orderProductRow}
+              columns={orderProductsCol}
+              pagination={10}
+              pageSize={10}
+            />
           </div>
         </div>
       </div>
