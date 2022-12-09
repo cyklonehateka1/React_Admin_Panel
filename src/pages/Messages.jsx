b2e3f0fcf1730dd68a8bc6571427984a1f8cf4ac
Emmanuel Hateka <emmanuelhateka.com>
2022-12-09 02:00:46 +0000

    Messages page in progress

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -1,7 +1,351 @@
 import React from "react";
-
+import TopBar from "../components/TopBar";
+import "../styles/messages.css";
 const Messages = () => {
-  return <div>Messages</div>;
+  return (
+    <div className="messages">
+      <TopBar />
+      <div className="messagesCont">
+        <div className="left">
+          <h5>Messages</h5>
+          <div className="messagesList">
+            <div className="messageBox activeChat">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+            <div className="messageBox">
+              <div className="left">
+                <div className="profileImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="textsCont">
+                  <span>Cyklone Hateka</span>
+                  <p>Please when will the new Nike...</p>
+                </div>
+              </div>
+              <div className="timeCountCont">
+                <span>19:14</span>
+                <div>2</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <div className="partnerCont">
+            <div>
+              <div>
+                <div className="partnerImgCont">
+                  <img
+                    src="https://i.ibb.co/dGcxdHw/intern-img-jg.png"
+                    alt=""
+                  />
+                </div>
+                <div className="partnerMessageCont">
+                  <p>Please when will the new Nike Airfoce 1 be in stock?</p>
+                </div>
+              </div>
+              <span>Dec 7, 2022, 19:30</span>
+            </div>
+          </div>
+          <div className="userCont">
+            <div>
+              <div>
+                <p>On the 21st of December 2022</p>
+              </div>
+              <span>Dec 7, 2022, 19:31</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Messages;
